467627b873f961eb2470653257d72a95b4cb050f
arshidhakt05-eng <arshidhakt05@gmail.com>
2026-04-22 13:59:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+student="arshi"
+console.log(name);
+
+var a=10
+console.log(a);
+
+ var b=20
+console.log(b);
+
+function addtwonumber(){
+    var a=10
+    var b=20
+    var sum=a+b
+    console.log(sum)
+
+}
+addtwonumber()
+
+function addtwonumber(a,b){
+    var sum=a+b
+    console.log(sum)    
+}
+addtwonumber(30,40)
+
+document.write("welcome to upcode")
+document.getElementById("name").innerHTML="welcome to javascript programminglanguage"
